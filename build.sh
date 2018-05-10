7097e1f208de4059ee913dfa0a21a2267fac1183
Jody L. Whitlock <jody.whitlock@tiberiansun.us>
2018-05-09 22:45:37 -0500

    Last minute tweaks

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t tibmeister/unifi:5.6.26-10236 .
-docker tag tibmeister/unifi:5.6.26-10236 tibmeister/unifi:latest
-docker push tibmeister/unifi:5.6.26-10236
+docker build -t tibmeister/unifi:5.6.29-10253 .
+docker tag tibmeister/unifi:5.6.29-10253 tibmeister/unifi:latest
+docker push tibmeister/unifi:5.6.29-10253
 docker push tibmeister/unifi:latest
