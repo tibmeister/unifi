5197fa746e25e24b582ffafa58d024f62915156a
Jody L. Whitlock <jody.whitlock@tiberiansun.us>
2018-05-10 00:15:19 -0500

    Cleanup

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t tibmeister/unifi:5.7.23-10670 .
-#docker tag tibmeister/unifi:5.7.23-10670 tibmeister/unifi:latest
-#docker push tibmeister/unifi:5.7.23-10670
-#docker push tibmeister/unifi:latest
