5401b6647f72ecf41347008d52288e02cd7bdf8e
Jody L. Whitlock <jody.whitlock@tiberiansun.us>
2020-04-27 14:23:45 -0500

    Updated to new version of Controller

--- gpgkey.sh
+++ gpgkey.sh
@@ -9,5 +9,6 @@ wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ubnt.com/unifi/unifi-re
 # for mongo-10gen [optional]
 #apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 68818C72E52529D4
 
 # or over HTTP by using hkp://keyserver.ubuntu.com:80
